7d2564027d7f0b51349195aeb319f68c0fbf48fe
emco3098 <emco3098@colorado.edu>
2025-03-20 02:01:11 -0600

    Update main.js

--- wa/wa10/main.js
+++ wa/wa10/main.js
@@ -11,7 +11,7 @@ function randomValueFromArray(array){
 
 let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
 
-let insertX = ["Wilnnie the cat", "Prime Time", "Chappel Roan"]
+let insertX = ["Winnie the cat", "Prime Time", "Chappel Roan"]
 
 let insertY = ["the St Louis Arch", "Disneyland", "Canada"]
 
